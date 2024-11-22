b01710638ca19769f717d59eb50d101877f390de
AYOPELUMI <ayopelumi2014@gmail.com>
2024-11-22 00:13:00 -0800

    worked on tsconfig.json

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { useState, useEffect } from 'react';
-import Image from 'next/image';
+
+// @ts-ignore
 import _ from 'lodash';
 import { motion } from 'framer-motion';
 import MovieCard from './components/MovieCard';
