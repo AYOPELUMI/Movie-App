9a025ae1ffe9bca1fecc3f9f7b9dd5b971208744
AYOPELUMI <ayopelumi2014@gmail.com>
2025-05-21 05:57:53 -0700

    adjuested the key

--- src/app/Hooks/fetchGenres.ts
+++ src/app/Hooks/fetchGenres.ts
@@ -5,7 +5,7 @@ import axios from 'axios';
 const fetchGenres = async () => {
     const response = await axios.get('https://api.themoviedb.org/3/genre/movie/list', {
         params: {
-            api_key: process.env.NEXT_PUBLIC_TMDB_API_KEYN,
+            api_key: process.env.NEXT_PUBLIC_TMDB_API_KEY,
             language: 'en',
         },
         headers: {
@@ -13,6 +13,7 @@ const fetchGenres = async () => {
             Authorization: `Bearer ${process.env.NEXT_PUBLIC_TMDB_API_ACCESS_TOKEN}`
         }
     });
+
     return response.data.genres; // The genres will be in response.data.genres
 };
 
